29b143f4f1abf5941d7c9106500a46cf9ad54f44
suti <lxy96@outlook.com>
2017-09-04 21:53:39 +0800

    add

--- bin/test.js
+++ bin/test.js
@@ -2,10 +2,15 @@ const gpio = require('pi-gpio')
 let flag=false
 
 gpio.open(8, "output", function(err) {		// Open pin 16 for output
-	gpio.write(8, 1, function() {			// Set pin 16 high (1)
-		gpio.close(8);						// Close pin 16
-	});
+	console.log(err)
 });
+setInterval(()=>{
+	gpio.write(8, flag?1:0, function() {			// Set pin 16 high (1)
+		// gpio.close(8);						// Close pin 16
+	});
+	flag=!flag
+	console.log('change! ::',flag?1:0)
+},1000)
 
 // setInterval(()=>{
 // 	gpio.write(8, flag?1:0, e=>{

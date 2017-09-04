b3cf24b25f1fd1d26082f4fccb23e134043e181c
suti <lxy96@outlook.com>
2017-09-04 21:36:45 +0800

    add

--- bin/test.js
+++ bin/test.js
@@ -1,8 +1,15 @@
 const gpio = require('pi-gpio')
 let flag=false
-setInterval(()=>{
-	gpio.write(8, flag?1:0, e=>{
-		console.log('pi-gpio-waring!!',e)
+
+gpio.open(8, "output", function(err) {		// Open pin 16 for output
+	gpio.write(8, 1, function() {			// Set pin 16 high (1)
+		gpio.close(8);						// Close pin 16
 	});
-	flag=!flag
-})
+});
+
+// setInterval(()=>{
+// 	gpio.write(8, flag?1:0, e=>{
+// 		console.log('pi-gpio-waring!!',e)
+// 	});
+// 	flag=!flag
+// },300)

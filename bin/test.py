a51b407e09392097fe85ec783d13878fa7b175e8
suti <lxy96@outlook.com>
2017-09-07 20:56:39 +0800

    update

--- bin/test.py
+++ bin/test.py
@@ -6,6 +6,16 @@ try:
 except RuntimeError:
     print("引入错误")
 
+def setTimeOutTo():
+    GPIO.output(16,Gpio_flag)
+    Gpio_flag=1 if False else 0
+    time.sleep(0.1)
+    setTimeOutTo()
+
+def watcher(v):
+    print (str(time.time())+':: '+v)
+
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
@@ -13,14 +23,6 @@ Gpio_flag=1
 
 GPIO.add_event_detect(18, GPIO.RISING,callback=watcher)
 
-def setTimeOutTo():
-    GPIO.output(16,Gpio_flag)
-    Gpio_flag=1 if False else 0
-    time.sleep(0.1)
-    setTimeOutTo()
-
-def watcher (v):
-    print (str(time.time())+':: '+v)
-
+setTimeOutTo()
 
 

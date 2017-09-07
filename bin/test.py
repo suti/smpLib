c0093e1310e5a1fcfb3c928ea46bc2709b57eaef
suti <lxy96@outlook.com>
2017-09-07 20:49:39 +0800

    update

---
+++ bin/test.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("引入错误")
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+Gpio_flag=1
+
+GPIO.add_event_detect(18, GPIO.RISING,callback=watcher)
+
+def setTimeOutTo():
+    GPIO.output(16,Gpio_flag)
+    Gpio_flag=1 if False else 0
+    time.sleep(0.1)
+    setTimeOutTo()
+
+def watcher (v):
+    print str(time.time())+':: '+v
+
+
+

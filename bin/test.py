ba6a4329ecd45c1d0f510c57ec18625e590795d7
suti <lxy96@outlook.com>
2017-09-07 21:14:26 +0800

    update

--- bin/test.py
+++ bin/test.py
@@ -1,49 +1,30 @@
-# import RPi.GPIO as GPIO
-# import time
-#
-# try:
-#     import RPi.GPIO as GPIO
-# except RuntimeError:
-#     print("引入错误")
-#
-# Gpio_flag = 1
-#
-# # def setTimeOutTo():
-# #     global Gpio_flag
-# #     GPIO.output(16,Gpio_flag)
-# #     Gpio_flag=1 if False else 0
-# #     time.sleep(0.1)
-# #     setTimeOutTo()
-#
-# # def watcher(v):
-# #     print (str(time.time())+':: '+v)
-#
-#
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setwarnings(False)
-#
-# # GPIO.setup(18,GPIO.IN)
-# GPIO.setup(16,GPIO.OUT)
-#
-# # GPIO.add_event_detect(18, GPIO.RISING,callback=watcher)
-#
-# # setTimeOutTo()
-#
-# while True:
-#     GPIO.output(16,1)
-#     time.sleep()
-
 import RPi.GPIO as GPIO
 import time
 
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("引入错误")
+
+Gpio_flag = 1
+
+def setTimeOutTo():
+    global Gpio_flag
+    GPIO.output(11,Gpio_flag)
+    Gpio_flag=1 if False else 0
+    time.sleep(0.1)
+    setTimeOutTo()
+
+def watcher(v):
+    print(v)
+
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setwarnings(False)
 
-while True:
-    GPIO.output(11, 1)
-    time.sleep(1)
-    GPIO.output(11, 0)
-    time.sleep(1)
+GPIO.setup(12,GPIO.IN)
+GPIO.setup(11,GPIO.OUT)
 
-GPIO.cleanup()
+GPIO.add_event_detect(12, GPIO.RISING,callback=watcher)
 
+setTimeOutTo()

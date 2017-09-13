6610685ca50923bce2d64bdb99125b75e00f4158
suti <lxy96@outlook.com>
2017-09-13 20:49:36 +0800

    update

--- bin/e.js
+++ bin/e.js
@@ -32,10 +32,10 @@ function strs(v) {
 			console.log(`[${i}][${j}]::${value}`)
 			if(value){
 				pi.digitalWrite(23,1)
-				pi.delay(150)
+				pi.delay(80)
 			}else {
 				pi.digitalWrite(23,1)
-				pi.delay(50)
+				pi.delay(20)
 			}
 			pi.digitalWrite(23,0)
 			pi.delay(20)

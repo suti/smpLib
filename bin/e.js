72c8898869a50118caac30bbbbf7b981148425e1
suti <lxy96@outlook.com>
2017-09-13 13:09:55 +0800

    update

--- bin/e.js
+++ bin/e.js
@@ -3,14 +3,8 @@ const pi=require('wiringpi-node')
 pi.setup('gpio')
 
 pi.pinMode(23,pi.OUTPUT)
-pi.pinMode(24,pi.INPUT)
-//pi.digitalWrite(15,0)
 let value=1
 
-pi.pullUpDnControl(24,pi.PUD_UP)
-pi.wiringPiISR(24,pi.INT_EDGE_FALLING,e=>{
-	console.log(`pin changed to low ${e}`)
-})
 
 while(true){
 	pi.digitalWrite(23,value)

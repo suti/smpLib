56cb5bfaadec2d499b816ce9dff252a49095395f
suti <lxy96@outlook.com>
2017-09-13 13:35:27 +0800

    update

--- bin/e.js
+++ bin/e.js
@@ -1,14 +1,25 @@
 const pi=require('wiringpi-node')
+const repl = require('repl')
 
 pi.setup('gpio')
 
 pi.pinMode(23,pi.OUTPUT)
 let value=1
 
+repl.start({
+	prompt: '请输入字符> ',
+	input: inputCtrl
+})
 
-while(true){
-	pi.digitalWrite(23,value)
-	value=+!value
-	pi.delay(100)
-//	console.log(Date.now())
+function inputCtrl(v) {
+	console.log(v)
+
+	repl.end()
+// 	while(true){
+// 		pi.digitalWrite(23,value)
+// 		value=+!value
+// 		pi.delay(100)
+// //	console.log(Date.now())
+// 	}
 }
+

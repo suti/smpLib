413c9cb478fab8667e8c397e0be757a60fb231eb
suti <lxy96@outlook.com>
2017-09-13 15:03:27 +0800

    update

--- bin/r.js
+++ bin/r.js
@@ -20,8 +20,8 @@ pi.pullUpDnControl(24,pi.PUD_UP)
 pi.wiringPiISR(24,pi.INT_EDGE_RISING,e=>{
 	if(time){
 		let delay=Date.now()-time
-		if(delay>150){
-			let n=((delay/100)|0)+((delay-((delay/100)|0)*100)>50?1:0),t=[]
+		if(delay>120){
+			let n=((delay/100)|0)+((delay-((delay/100)|0)*100)>100?1:0),t=[]
 			for(let i=0;i<n;i++)
 				t.push(0)
 			arr.push(...t,1)
@@ -33,14 +33,15 @@ pi.wiringPiISR(24,pi.INT_EDGE_RISING,e=>{
 		time=Date.now()
 		arr.push(1)
 	}
-	console.log(arr)
 })
 
 function dataCtrl(v) {
 	console.log(v)
-	if(v.length!==8) return
-	let arrs=v,code
-	arr=[]
+	if(v.length<=8) return
+	let arrs=[],code
+	for(let i=0;i<8;i++){
+		arrs.push(arr.shift())
+	}
 	code=parseInt(arrs.json(''),8)
 	console.log(code,String.fromCharCode(code))
 }

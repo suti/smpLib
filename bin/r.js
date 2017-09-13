731af290b823bf9328853b0e73cf06e7f008f6e2
suti <lxy96@outlook.com>
2017-09-13 15:52:28 +0800

    update

--- bin/r.js
+++ bin/r.js
@@ -47,7 +47,7 @@ function dataCtrl(v) {
 	for(let i=0;i<8;i++){
 		arrs.push(data.arr.shift())
 	}
-	code=parseInt(arrs.join(''),8)
+	code=parseInt(arrs.join(''),2)
 	console.log(code,String.fromCharCode(code))
 }
 
